a2eb8198118ed4caa780757f9e1de5f459160079
zonaetmunna <zonaetmonna@gmail.com>
2023-05-11 22:25:36 +0600

    add

--- src/view/pages/main/Checkout.jsx
+++ src/view/pages/main/Checkout.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Controller, useForm, useWatch } from "react-hook-form";
+import { useSelector } from "react-redux";
 
 const Checkout = () => {
   const {
@@ -19,6 +20,8 @@ const Checkout = () => {
     }
   }, [email]);
 
+  const { cart, total, subTotal } = useSelector((state) => state.cart);
+
   const onSubmit = (data) => {
     console.log(data);
     reset();
@@ -32,20 +35,26 @@ const Checkout = () => {
       <div className="px-2 py-2">
         <hr />
       </div>
-      <div class="flex justify-between">
-        <div class="w-1/2 p-4 bg-gray-100">
+      <div class="flex justify-between px-5 ">
+        <div class="w-1/2 p-4 bg-gray-100 mx-5 shadow-lg">
           {/* Left section content goes here  */}
-          <div>
-            <h4>Billing Details</h4>
+          <div className="border-b mb-6 pb-4 text-center">
+            <h2 className="text-lg leading-6 font-medium text-gray-900">
+              Billing Details
+            </h2>
           </div>
-          <form onSubmit={handleSubmit(onSubmit)}>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="bg-white  rounded px-8 pt-6 pb-8 mb-4"
+          >
             <div className="flex flex-wrap -mx-3 mb-6">
               {/* first name */}
-              <div className="flex flex-wrap -mx-3 mb-6">
-                <div className="w-full px-3 mb-6 md:mb-0">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+                {/* first name */}
+                <div className="mb-4">
                   <label
                     htmlFor="firstName"
-                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    className="block text-gray-700 text-sm font-bold mb-2"
                   >
                     First Name
                   </label>
@@ -54,7 +63,7 @@ const Checkout = () => {
                     name="firstName"
                     id="firstName"
                     {...register("firstName", { required: true })}
-                    className={`appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white ${
+                    className={`appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
                       errors.firstName && "border-red-500"
                     }`}
                   />
@@ -64,10 +73,12 @@ const Checkout = () => {
                     </p>
                   )}
                 </div>
-                <div className="w-full px-3">
+
+                {/* last name */}
+                <div className="mb-4">
                   <label
                     htmlFor="lastName"
-                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    className="block text-gray-700 text-sm font-bold mb-2"
                   >
                     Last Name
                   </label>
@@ -76,7 +87,7 @@ const Checkout = () => {
                     name="lastName"
                     id="lastName"
                     {...register("lastName", { required: true })}
-                    className={`appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white ${
+                    className={`appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
                       errors.lastName && "border-red-500"
                     }`}
                   />
@@ -86,13 +97,13 @@ const Checkout = () => {
                     </p>
                   )}
                 </div>
-              </div>
-              {/* email */}
-              <div className="flex flex-wrap -mx-3 mb-6">
-                <div className="w-full px-3">
+
+                {/* email */}
+                {/* email */}
+                <div className="mb-4">
                   <label
                     htmlFor="email"
-                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    className="block text-gray-700 text-sm font-bold mb-2"
                   >
                     Email Address
                   </label>
@@ -104,7 +115,7 @@ const Checkout = () => {
                       required: true,
                       pattern: /^\S+@\S+$/i,
                     })}
-                    className={`appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white ${
+                    className={`appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
                       errors.email && "border-red-500"
                     }`}
                   />
@@ -119,14 +130,13 @@ const Checkout = () => {
                     </p>
                   )}
                 </div>
-              </div>
 
-              {/* number */}
-              <div className="flex flex-wrap mx-3 mb-6">
-                <div className="w-full px-3">
+                {/* number */}
+                {/* phone */}
+                <div className="mb-4">
                   <label
-                    className="block mb-2 font-bold text-gray-700"
                     htmlFor="phone"
+                    className="block text-gray-700 text-sm font-bold mb-2"
                   >
                     Phone <span className="text-red-500">*</span>
                   </label>
@@ -140,17 +150,17 @@ const Checkout = () => {
                     {...register("phone", { required: true })}
                   />
                   {errors.phone && (
-                    <span className="text-red-500">This field is required</span>
+                    <p className="text-red-500 text-xs italic">
+                      This field is required
+                    </p>
                   )}
                 </div>
-              </div>
 
-              {/* address */}
-              <div className="flex flex-wrap -mx-3 mb-6">
-                <div className="w-full px-3">
+                {/* address */}
+                <div className="mb-4">
                   <label
                     htmlFor="address"
-                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    className="block text-gray-700 font-bold mb-2"
                   >
                     Address
                   </label>
@@ -159,31 +169,30 @@ const Checkout = () => {
                     name="address"
                     id="address"
                     {...register("address", { required: true })}
-                    className={`appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white ${
+                    className={`w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
                       errors.address && "border-red-500"
                     }`}
+                    placeholder="123 Main St."
                   />
                   {errors.address && (
-                    <p className="text-red-500 text-xs italic">
+                    <p className="mt-2 text-sm text-red-600">
                       Address is required
                     </p>
                   )}
                 </div>
-              </div>
-              {/* country */}
-              <div className="flex flex-wrap mx-3 mb-6">
-                <div className="w-full px-3">
+                {/* country */}
+                <div className="flex flex-col mb-6">
                   <label
-                    className="block mb-2 font-bold text-gray-700"
                     htmlFor="country"
+                    className="text-gray-700 font-bold mb-2"
                   >
                     Country <span className="text-red-500">*</span>
                   </label>
                   <select
                     name="country"
                     id="country"
-                    className={`appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
-                      errors.country ? "border-red-500" : ""
+                    className={`w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
+                      errors.country && "border-red-500"
                     }`}
                     {...register("country", { required: true })}
                   >
@@ -193,94 +202,136 @@ const Checkout = () => {
                     {/* add more countries here */}
                   </select>
                   {errors.country && (
-                    <span className="text-red-500">This field is required</span>
-                  )}
-                </div>
-              </div>
-
-              {/* city and zip */}
-              <div className="flex flex-wrap -mx-3 mb-6">
-                <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                  <label
-                    htmlFor="city"
-                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  >
-                    City
-                  </label>
-                  <input
-                    type="text"
-                    name="city"
-                    id="city"
-                    {...register("city", { required: true })}
-                    className={`appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white ${
-                      errors.city && "border-red-500"
-                    }`}
-                  />
-                  {errors.city && (
-                    <p className="text-red-500 text-xs italic">
-                      City is required
+                    <p className="mt-2 text-sm text-red-600">
+                      Country is required
                     </p>
                   )}
                 </div>
-                <div className="w-full md:w-1/2 px-3">
-                  <label
-                    htmlFor="zip"
-                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  >
-                    Zip Code
-                  </label>
-                  <input
-                    type="text"
-                    name="zip"
-                    id="zip"
-                    {...register("zip", {
-                      required: true,
-                      pattern: /^[0-9]{5}(?:-[0-9]{4})?$/i,
-                    })}
-                    className={`appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white ${
-                      errors.zip && "border-red-500"
-                    }`}
-                  />
-                  {errors.zip && errors.zip.type === "required" && (
-                    <p className="text-red-500 text-xs italic">
-                      Zip code is required
-                    </p>
-                  )}
-                  {errors.zip && errors.zip.type === "pattern" && (
-                    <p className="text-red-500 text-xs italic">
-                      Invalid zip code
-                    </p>
-                  )}
+
+                {/* city and zip */}
+                <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+                  <div className="mb-6">
+                    <label
+                      htmlFor="city"
+                      className="block text-gray-700 font-bold mb-2"
+                    >
+                      City
+                    </label>
+                    <input
+                      type="text"
+                      name="city"
+                      id="city"
+                      {...register("city", { required: true })}
+                      className={`w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
+                        errors.city && "border-red-500"
+                      }`}
+                      placeholder="Los Angeles"
+                    />
+                    {errors.city && (
+                      <p className="mt-2 text-sm text-red-600">
+                        City is required
+                      </p>
+                    )}
+                  </div>
+                  <div className="mb-6">
+                    <label
+                      htmlFor="zip"
+                      className="block text-gray-700 font-bold mb-2"
+                    >
+                      Zip Code
+                    </label>
+                    <input
+                      type="text"
+                      name="zip"
+                      id="zip"
+                      {...register("zip", {
+                        required: true,
+                        pattern: /^[0-9]{5}(?:-[0-9]{4})?$/i,
+                      })}
+                      className={`w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
+                        errors.zip && "border-red-500"
+                      }`}
+                      placeholder="90210"
+                    />
+                    {errors.zip && errors.zip.type === "required" && (
+                      <p className="mt-2 text-sm text-red-600">
+                        Zip code is required
+                      </p>
+                    )}
+                    {errors.zip && errors.zip.type === "pattern" && (
+                      <p className="mt-2 text-sm text-red-600">
+                        Invalid zip code
+                      </p>
+                    )}
+                  </div>
                 </div>
-              </div>
-
-              <div className="flex flex-col items-start">
-                <input
-                  type="radio"
-                  id="billing"
-                  value={true}
-                  {...register("billing")}
-                  required
-                />
-                <label className="ml-2 text-lg" htmlFor="available">
-                  billing Details
-                </label>
-              </div>
-
-              <div className="!mt-8 ">
-                <button
-                  type="submit"
-                  className="py-2 px-4 mx-3 my-3 bg-blue-500 text-white font-bold rounded hover:bg-blue-700"
-                  disabled={disabled}
-                >
-                  Sign up
-                </button>
               </div>
             </div>
           </form>
         </div>
-        <div class="w-1/2 p-4 bg-gray-200">
-          {/* <!-- Right section content goes here --> */}
+        <div className="w-1/2 p-4 bg-gray-100 rounded-lg shadow-lg">
+          <h2 className="text-2xl font-bold mb-4">Your Cart</h2>
+          {cart.map((item) => (
+            <div
+              key={item.id}
+              className="flex justify-between items-center py-2"
+            >
+              <div className="font-semibold">{item.name}</div>
+              <div className="text-gray-600">${item.price.toFixed(2)}</div>
+              <div className="text-gray-600">x {item.quantity}</div>
+            </div>
+          ))}
+          <div className="border-t-2 border-gray-200 py-2">
+            <div className="flex justify-between items-center font-semibold">
+              <div>Total:</div>
+              <div>${total.toFixed(2)}</div>
+            </div>
+          </div>
+          <div className="mt-6">
+            <h3 className="font-medium mb-2">Payment Method</h3>
+            <div className="flex items-center mb-4">
+              <input
+                type="radio"
+                name="paymentMethod"
+                id="directBankTransfer"
+                className="mr-2"
+              />
+              <label htmlFor="directBankTransfer" className="font-medium">
+                Direct Bank Transfer
+              </label>
+            </div>
+            <p>
+              Make your payment directly into our bank account. Please use your
+              Order ID as the payment reference.
+            </p>
+
+            <div className="flex items-center mb-4">
+              <input
+                type="radio"
+                name="paymentMethod"
+                id="cashOnDelivery"
+                className="mr-2"
+              />
+              <label htmlFor="cashOnDelivery" className="font-medium">
+                Cash on Delivery
+              </label>
+            </div>
+
+            <div className="flex items-center mb-4">
+              <input
+                type="radio"
+                name="paymentMethod"
+                id="creditDebitCards"
+                className="mr-2"
+              />
+              <label htmlFor="creditDebitCards" className="font-medium">
+                Credit/Debit Cards or Paypal
+              </label>
+            </div>
+          </div>
+          <button className="bg-indigo-600 text-white px-4 py-2 rounded-lg mt-4 hover:bg-indigo-500">
+            Order
+          </button>
         </div>
       </div>
     </div>
