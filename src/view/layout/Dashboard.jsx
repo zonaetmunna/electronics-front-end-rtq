d9f6eb85cb770e8b3da290015626b36fb6d36270
zonaetmunna <zonaetmonna@gmail.com>
2023-08-26 18:55:32 +0600

    blog add

--- src/view/layout/Dashboard.jsx
+++ src/view/layout/Dashboard.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import DashboardNavbar from "../components/common/NavigationBar/DashboardNavbar";
-import DashboardSidebar from "../components/common/Sidebar/DashboardSidebar";
+
 const Dashboard = () => {
   return (
     <div>
       <DashboardNavbar />
       <div className="flex h-screen">
-        <DashboardSidebar />
+        {/* Sidebar */}
         <div className="flex-grow bg-gray-100 p-6">
           <Outlet />
         </div>
