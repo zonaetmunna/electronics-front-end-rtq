3c9daadb3e1f2cda113a98ec4148a68ba7d21332
zonaetmunna <zonaetmonna@gmail.com>
2023-09-15 02:29:17 +0600

    blog modify

---
+++ src/view/components/common/Skeleton/SkeletonBlogCard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const SkeletonBlogCard = () => {
+  return (
+    <div className="bg-gray-200 shadow-md rounded-md overflow-hidden animate-pulse">
+      <div className="w-full h-64 bg-gray-300"></div>
+      <div className="px-4 py-6">
+        <div className="w-3/4 h-4 mb-2 bg-gray-300"></div>
+        <div className="w-full h-6 mb-2 bg-gray-300"></div>
+        <div className="w-full h-6 mb-2 bg-gray-300"></div>
+        <div className="flex justify-between">
+          <div className="w-1/4 h-6 bg-gray-300"></div>
+          <div className="w-1/4 h-6 bg-gray-300"></div>
+        </div>
+        <div className="w-1/2 h-8 mt-4 bg-gray-300"></div>
+      </div>
+    </div>
+  );
+};
+
+export default SkeletonBlogCard;
--- src/view/pages/main/Blog.jsx
+++ src/view/pages/main/Blog.jsx
@@ -1,99 +1,28 @@
 import React from "react";
-import { FaTags, FaUser } from "react-icons/fa";
-import { RiCalendarLine } from "react-icons/ri";
-import { Link } from "react-router-dom";
 import { useGetBlogsQuery } from "../../../features/blog/blogApi";
-
+import BlogCard from "../../components/common/Card/BlogCard";
+import SkeletonBlogCard from "../../components/common/Skeleton/SkeletonBlogCard";
 const Blog = () => {
   const { data: blogs, isLoading, isError, isSuccess } = useGetBlogsQuery({});
   const blogsData = blogs?.data;
   console.log(blogsData);
-  /* const blogs = [
-    {
-      id: 1,
-      title: "Top 10 fashion trends for summer 2022",
-      date: "June 15, 2022",
-      author: "Jane Doe",
-      category: "Fashion",
-      image: "/images/blog-1.jpg",
-      excerpt:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco...",
-    },
-    {
-      id: 2,
-      title: "New home decor collection now available",
-      date: "May 25, 2022",
-      author: "John Doe",
-      category: "Home Decor",
-      image: "/images/blog-2.jpg",
-      excerpt:
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore...",
-    },
-    {
-      id: 3,
-      title: " decor collection now available",
-      date: "May 25, 2022",
-      author: "John Doe",
-      category: "Home Decor",
-      image: "/images/blog-3.jpg",
-      excerpt:
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore...",
-    },
-    {
-      id: 4,
-      title: " decor collection now available",
-      date: "May 25, 2022",
-      author: "John Doe",
-      category: "Home Decor",
-      image: "/images/blog-3.jpg",
-      excerpt:
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore...",
-    },
-    // add more blogs here
-  ]; */
+
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {blogsData?.map((blog) => (
-          <div
-            key={blog._id}
-            className="bg-white shadow-md rounded-md overflow-hidden"
-          >
-            <img
-              src={blog.image}
-              alt={blog.title}
-              className="w-full h-64 object-cover"
-            />
-            <div className="px-4 py-6">
-              <div className="text-gray-500 text-sm mb-2">
-                <RiCalendarLine className="inline-block mr-2" />
-                {blog.createdAt}
-              </div>
-              <h3 className="text-lg font-medium text-gray-800 mb-2">
-                {blog.title}
-              </h3>
-
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <FaUser className="inline-block mr-1" />
-                  <span className="text-gray-700 text-sm">
-                    {blog?.author?.firstName}
-                  </span>
-                </div>
-                <div className="flex items-center">
-                  <FaTags className="inline-block mr-1" />
-                  <span className="text-gray-700 text-sm">{blog.category}</span>
-                </div>
-              </div>
-              <Link
-                to={`/blog/${blog._id}`}
-                className="text-blue-500 hover:text-blue-600 font-medium mt-4"
-              >
-                Read More
-              </Link>
-            </div>
-          </div>
-        ))}
+        {isLoading ? (
+          // Display loading skeleton cards while data is being fetched
+          Array.from({ length: 6 }).map((_, index) => (
+            <SkeletonBlogCard key={index} />
+          ))
+        ) : isError ? (
+          <p>Error fetching data</p>
+        ) : isSuccess && blogsData ? (
+          // Display actual blog cards once data is available
+          blogsData.map((blog) => <BlogCard blog={blog} key={blog._id} />)
+        ) : (
+          <p>No blogs found</p>
+        )}
       </div>
     </div>
   );
