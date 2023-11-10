e0438abe5a1e8c4107804010f2d2b23c041113eb
zonaetmunna <zonaetmonna@gmail.com>
2023-11-11 02:11:22 +0600

    modify

--- src/view/layout/Dashboard.jsx
+++ src/view/layout/Dashboard.jsx
@@ -1,21 +1,20 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import DashboardNavbar from "../components/common/NavigationBar/DashboardNavbar";
-import DashboardSidebar from "../components/common/Sidebar/DashboardSidebar";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import DashboardNavbar from '../components/common/NavigationBar/DashboardNavbar';
+import DashboardSidebar from '../components/common/Sidebar/DashboardSidebar';
 
 const Dashboard = () => {
-  return (
-    <div>
-      <DashboardNavbar />
-      <div className="flex h-screen">
-        {/* Sidebar */}
-        <DashboardSidebar />
-        <div className="flex-grow bg-gray-100 p-6 shadow-md rounded-md ml-5">
-          <Outlet />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<>
+			<DashboardNavbar />
+			<div className='flex h-screen'>
+				<DashboardSidebar />
+				<div className='flex-grow bg-gray-100 p-6 shadow-md rounded-md ml-5'>
+					<Outlet />
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Dashboard;
--- src/view/layout/Error.jsx
+++ src/view/layout/Error.jsx
@@ -1,21 +1,19 @@
-import React from "react";
-import { useRouteError } from "react-router-dom";
+import React from 'react';
+import { useRouteError } from 'react-router-dom';
 
 const Error = () => {
-  const error = useRouteError();
-  return (
-    <div className="bg-red-100 p-4 rounded-lg shadow-md">
-      <h1 className="text-3xl text-red-600 font-bold">Oops!</h1>
-      <p className="text-lg text-gray-800">
-        Sorry, an unexpected error has occurred.
-      </p>
-      {error && (
-        <p className="text-sm text-gray-600 mt-2">
-          <i>{error.statusText || error.message}</i>
-        </p>
-      )}
-    </div>
-  );
+	const error = useRouteError();
+	return (
+		<div className='bg-red-100 p-4 rounded-lg shadow-md'>
+			<h1 className='text-3xl text-red-600 font-bold'>Oops!</h1>
+			<p className='text-lg text-gray-800'>Sorry, an unexpected error has occurred.</p>
+			{error && (
+				<p className='text-sm text-gray-600 mt-2'>
+					<i>{error.statusText || error.message}</i>
+				</p>
+			)}
+		</div>
+	);
 };
 
 export default Error;
